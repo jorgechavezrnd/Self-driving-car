345ab9afeb71fd8804ee8e00ab2f727889026f68
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-06-19 23:36:26 -0400

    Example of 30 rays

--- sensor.js
+++ sensor.js
@@ -1,8 +1,8 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength = 100;
+        this.rayCount = 30;
+        this.rayLength = 150;
         this.raySpread = Math.PI / 4;
 
         this.rays = [];
