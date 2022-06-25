cd153bafd9ec638cc279bedda7c375eb3696d87b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-06-25 10:46:39 -0400

    Change colors of car and traffic

--- car.js
+++ car.js
@@ -108,11 +108,11 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx) {
+    draw(ctx, color) {
         if (this.damaged) {
             ctx.fillStyle = "gray";
         } else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
