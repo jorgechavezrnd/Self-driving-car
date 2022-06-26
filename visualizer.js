4074b333b2d96abd7f43c288a97377a4a2463dc2
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-06-26 00:48:31 -0400

    Add colors to connections

--- visualizer.js
+++ visualizer.js
@@ -16,7 +16,7 @@ class Visualizer {
         const right = left+width;
         const bottom = top+height;
 
-        const {inputs, outputs} = level;
+        const {inputs, outputs, weights} = level;
 
         for (let i = 0; i < inputs.length; i++) {
             for (let j = 0; j < outputs.length; j++) {
@@ -30,7 +30,12 @@ class Visualizer {
                     top
                 );
                 ctx.lineWidth = 2;
-                ctx.strokeStyle = "orange";
+                const value = weights[i][j];
+                const alpha = Math.abs(value);
+                const R = value<0 ? 0 : 255;
+                const G = R;
+                const B = value>0 ? 0 : 255;
+                ctx.strokeStyle = "rgba("+R+","+G+","+B+","+alpha+")";
                 ctx.stroke();
             }
         }
